bd31d764880ff3fe413f32ac90d6cd49e045641a
KiraKostina <kostina.kira@gmail.com>
2024-02-23 13:56:51 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -2,28 +2,37 @@ const inputAmount = document.querySelector('#controls > input');
 const createBtn = document.querySelector('button[data-create]');
 const destroyBtn = document.querySelector('button[data-destroy]');
 const boxes = document.querySelector('#boxes');
+const color = getRandomHexColor();
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-const color = getRandomHexColor();
 
-
-createBtn.addEventListener("click", event => {
-    if (inputAmount >= 0 && inputAmount <= 100) { 
-        function createBoxes(amount) {
-            
-
-    }
-    }
-    
-
- })
-
-destroyBtn.addEventListener("click", event => { 
-
+createBtn.addEventListener("click", (event) => {
+  if (inputAmount.value > 0 && inputAmount.value <= 100) {
+    createBoxes(inputAmount.value)
+  }
+  else { alert("З такою кількістю не працює!") }
 })
 
-// .innerHTML = очищення
+let boxsize;
+let newDiv;
+function createBoxes(amount) {
+  boxes.innerHTML = "";
+ boxsize = 30;
+  for (let i = 1; i <= amount; i += 1) {
+    newDiv = `<div style="width: ${boxsize}px; height: ${boxsize}px; background: ${getRandomHexColor()}"></div>`;
+    boxes.insertAdjacentHTML("beforeend", newDiv);
+    boxsize += 10;
+  }
+}
+
+destroyBtn.addEventListener("click", (event) => { 
+  inputAmount.value = ""
+boxes.innerHTML = ""
+})
+
+
+
